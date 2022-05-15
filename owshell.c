240454f3c64cd8b43161eda601fc795c983e90a8
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-15 23:37:28 +0300

    Owshell

---
+++ owshell.c
@@ -0,0 +1,51 @@
+#include "sshell.h"
+/**
+  * main - program gateway
+  * @ac: num of av
+  * @av: command line args passed
+  *
+  * Return: Always 0
+  */
+int main(__attribute__((unused))int ac, char **av)
+{
+  char *line;
+  size_t n;
+  int count = 0;
+  
+  signal(SIGINT, SIG_IGN);
+  do {
+    count++;
+    line = NULL;
+    n = 0;
+    _parse(line, n, count, av);
+  } while (1);
+  return (0);
+}
+/**
+  *
+  */
+void _parse(char *line, size_t n, int count, char **av)
+{
+  int i, tkn_count = 0;
+  ssize_t get_ln;
+  char **s_array;
+  const char *delim = " \n\t";
+  
+  write(1, PROMPT, _strlen(PROMPT));
+  get_ln = getline(&line, &n, stdin);
+  if (get_ln != -1) {
+    s_array = strtok_array(line, delim, tkn_count);
+    if (s_array[0] == NULL) {
+      _free(2, s_arrray, line);
+      return;
+    }
+    i = built_ins(s_array, line);
+    if (i == -1)
+      fork_a_child(s_array, line, count, av);
+    for (i = 0; s_array[i] != NULL; i++)
+      free(s_array[i]);
+    _free(2, s_array, line);
+  }
+  else
+    _exit_(line);
+}
