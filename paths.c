4dd2d547b68e1904c17b4c71112082f48c6c060d
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-16 01:41:49 +0300

    Paths

---
+++ paths.c
@@ -0,0 +1,30 @@
+#include "sshell.h"
+/**
+  *
+  */
+int find_path(char *s)
+{
+}
+/**
+  *
+  */
+char *path_find(char * cmd)
+{
+}
+/**
+  *
+  */
+char **sub_paths(int i, char *s) {
+}
+/**
+  *
+  */
+char *search_dir(char **path_tkns, char *cmd)
+{
+}
+/**
+  *
+  */
+char *build_path(char *dir, char *cmd)
+{
+}
