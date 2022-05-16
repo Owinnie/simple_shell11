4b114e0452791f6c17952f82c62e6f7269045b9e
Owinnie <tillawinnie@gmail.com>
2022-05-16 05:02:45 -0700

    Free

--- free.c
+++ free.c
@@ -1,13 +1,33 @@
 #include "sshell.h"
 /**
-  *
+  * _free_ - free ptrs
+  * @freed: ptrs to be freed
   */
 void _free_(char **freed)
 {
+	int i;
+
+	for (i = 0; freed[i] != NULL; i++)
+		free(freed[i]);
+	free(freed);
 }
 /**
-  *
+  * _free - free ptrs to strs
+  * @n: num of ptrs to free
   */
 void _free(int n, ...)
 {
+	int i;
+	char *str;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char*);
+		if (str == NULL)
+			str = "(nil)";
+		free(str);
+	}
+	va_end(list);
 }
