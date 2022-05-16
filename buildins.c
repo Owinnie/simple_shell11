38b7f4c8139d2ab98862839e5ac44b3f6929e558
Owinnie <tillawinnie@gmail.com>
2022-05-16 04:35:53 -0700

    Builtins'

--- buildins.c
+++ buildins.c
@@ -8,15 +8,15 @@
   */
 int built_ins(char **a_rray, char *line)
 {
-  void (*built)(char *);
-  
-  built = list_built_ins(a_rray[0]);
-  if (built == NULL)
-    return (-1);
-  if (_strcmp("exit", a_rray[0]) == 0)
-    _free_(a_rray);
-  built(line);
-  return (0);
+	void (*built)(char *);
+
+	built = list_built_ins(a_rray[0]);
+	if (built == NULL)
+		return (-1);
+	if (_strcmp("exit", a_rray[0]) == 0)
+		_free_(a_rray);
+	built(line);
+	return (0);
 }
 /**
   * list_built_ins - list of builtins
@@ -25,20 +25,22 @@ int built_ins(char **a_rray, char *line)
   */
 void (*list_built_ins(char *str))(char *str)
 {
-  int i;
-  
-  blt builD[] = {
-    {"exit", _exit_},
-    {"env", _env},
-    {"cd", _cd},
-    {NULL, NULL}
-  };
-  for (i = 0; builD[i].built != NULL; i++) {
-    if (_strcmp(str, builD[i].built) == 0) {
-      return (builD[i].func);
-    }
-  }
-  return (NULL);
+	int i;
+
+	blt builD[] = {
+		{"exit", _exit_},
+		{"env", _env},
+		{"cd", _cd},
+		{NULL, NULL}
+	};
+	for (i = 0; builD[i].built != NULL; i++)
+	{
+		if (_strcmp(str, builD[i].built) == 0)
+		{
+			return (builD[i].func);
+		}
+	}
+	return (NULL);
 }
 /**
   * _exit_ - exit owshell
@@ -46,9 +48,9 @@ void (*list_built_ins(char *str))(char *str)
   */
 void _exit_(char *line)
 {
-  free(line);
-  write(1, "\n", 1);
-  exit(1);
+	free(line);
+	write(1, "\n", 1);
+	exit(1);
 }
 /**
   * _env - print environment vars
@@ -56,37 +58,42 @@ void _exit_(char *line)
   */
 void _env(__attribute__((unused))char *line)
 {
-  int i, j;
-  
-  for (i = 0; environ[i] != NULL; i++) {
-    for (j = 0; environ[i][j] != '\0'; j++)
-      write(1, &environ[i][j], 1);
-    write(1, "\n", 1);
-  }
+	int i, j;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; environ[i][j] != '\0'; j++)
+			write(1, &environ[i][j], 1);
+		write(1, "\n", 1);
+	}
 }
 /**
   * _cd - change dir
   * @line: user input
   */
-void _cd(char *line) {
-  int i, tkn_count = 0;
-  char **s_array;
-  const char *delim = " \n\t";
-  
-  s_array = strtok_array(line, delim, tkn_count);
-  if (s_array[0] == NULL) {
-    _free(2, s_array, line);
-    return;
-  }
-  if (s_array[1] == NULL) {
-    i = find_path("HOME");
-    chdir((environ[i]) + 5);
-  }
-  else if (_strcmp(s_array[1], "-") == 0) {
-    write(1, s_array[1], _strlen(s_array[1]));
-    write(1, "\n", 1);
-  }
-  else
-    chdir(s_array[1]);
-  _free_(s_array);
+void _cd(char *line)
+{
+	int i, tkn_count = 0;
+	char **s_array;
+	const char *delim = " \n\t";
+
+	s_array = strtok_array(line, delim, tkn_count);
+	if (s_array[0] == NULL)
+	{
+		_free(2, s_array, line);
+		return;
+	}
+	if (s_array[1] == NULL)
+	{
+		i = find_path("HOME");
+		chdir((environ[i]) + 5);
+	}
+	else if (_strcmp(s_array[1], "-") == 0)
+	{
+		write(1, s_array[1], _strlen(s_array[1]));
+		write(1, "\n", 1);
+	}
+	else
+		chdir(s_array[1]);
+	_free_(s_array);
 }
